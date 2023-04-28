692cd3175e77a74a27285a80fef01826d4fcfd04
ingdex <871958943@qq.com>
2023-04-28 09:48:10 +0800

    add script

--- distribution/bin/test-sync-async-producer.sh
+++ distribution/bin/test-sync-async-producer.sh
@@ -1,25 +1,42 @@
 #!/bin/bash
 
-export testTarget="sync-rocketmq"
+export testTarget="rocketmq"
 export testMyRocketMQ=0
 
-export originRocketMQPath="/root/rocketmq-4.9.4/"
+export RocketMQPath="/root/rocketmq-4.9.4/"
 export myRocketMQPath="/root/rocketmq/distribution/target/rocketmq-4.9.4/rocketmq-4.9.4/"
-export rocketMQLogDir="/root/data"
-export rocketMQProcessName="BrokerStartup"
+export RocketMQLogDir="/root/store"
+export RocketMQProcessName="BrokerStartup"
+
+export RocketMQPort=10911
+
+shutdownRocketMQ0() {
+  PID=$(jps | grep $RocketMQProcessName | grep -v grep | awk '{print $1}')
+  kill -9 $PID
+  sleep 5s
+  rm -r $RocketMQLogDir/*
+  sleep 1s
+}
+
+shutdownRocketMQ() {
+  shutdownRocketMQ0
+  # 需要检查的端口号
+  # 检查指定端口是否被占用
+  while true; do
+    RocketMQStatus=$(lsof -i:$RocketMQPort | grep LISTEN)
+    # 如果端口被占用
+    if [ -z "$RocketMQStatus" ]; then
+      break
+    fi
+    shutdownRocketMQ0
+  done
+}
 
 restartRocketMQ() {
-  PID=$(jps | grep $rocketMQProcessName | grep -v grep | awk '{print $1}')
-  kill -9 $PID
-  sleep 10s
-  rm -r $rocketMQLogDir/rocketmq
-  rm -r /root/logs/*
-  rm -r /root/store/*
-  sleep 10s
-  cd "$1/bin"
+  shutdownRocketMQ
+  cd $RocketMQPath/bin
   echo $(date) >> nohup.out
-  nohup sh mqbroker -c $1/conf/broker.conf -n 192.168.0.181:9876 >> nohup.out &
-  sleep 30s
+  nohup sh mqbroker -c $RocketMQPath/conf/broker.conf -n 192.168.0.181:9876 >> nohup.out &
 }
 
 doTest() {
@@ -30,58 +47,28 @@ doTest() {
   # RMQProducerPerf
   PID=$(ps -ef | grep "RMQProducerPerf" | grep -v grep | awk '{print $2}')
   kill -9 $PID
-  sleep 20s
+  sleep 10s
   # 获取输出文件中第2到11行以"Current Time: "开头的内容，并将结果打印出来
   grep -o '^Current Time: .*' output.log >> $2
 }
 
 export path=$(pwd)
 
-if jps | grep -v grep | grep $rocketMQProcessName >/dev/null; then
-  echo "BrokerStartup进程存在"
-else
-  echo "BrokerStartup进程不存在，启动BrokerStartup"
-  if [ $testMyRocketMQ -eq 1 ]; then
-    restartRocketMQ $myRocketMQPath
-  else
-    restartRocketMQ $originRocketMQPath
-  fi
-fi
+####################
+# sync rocketmq
+testTarget="sync-rocketmq"
+cp /root/broker-sync.conf $RocketMQPath/conf/broker.conf
+restartRocketMQ
 
-# sync
+# async producer
 # 外层循环遍历数字i，i的取值为8、16、32、64
-for i in 8 16 32 64; do
+for i in 1 4 8; do
   # 内层循环遍历数字j，j的取值为1024、4096、8192、16384、32768、65536、131072、1048576
   for j in 1024 4096 8192 16384 32768 65536 131072 1048576; do
-    USAGE=$(df -h $rocketMQLogDir | awk '{print $5}' | tail -n 1 | sed 's/%//')
+    USAGE=$(df -h $RocketMQLogDir | awk '{print $5}' | tail -n 1 | sed 's/%//')
     if [ $USAGE -gt 50 ]; then
       echo "已用存储空间大于50%！重启BrokerStartup"
-      if [ $testMyRocketMQ -eq 1 ]; then
-        restartRocketMQ $myRocketMQPath
-      else
-        restartRocketMQ $originRocketMQPath
-      fi
-    fi
-    configFilename="rmq$i-1-$j-sync.json"
-    resultFilename="result-$testTarget-sync-producer.txt"
-    cd $path
-    doTest $configFilename $resultFilename
-  done
-done
-
-# async
-# 外层循环遍历数字i，i的取值为8、16、32、64
-for i in 8 16 32 64; do
-  # 内层循环遍历数字j，j的取值为1024、4096、8192、16384、32768、65536、131072、1048576
-  for j in 1024 4096 8192 16384 32768 65536 131072 1048576; do
-    USAGE=$(df -h $rocketMQLogDir | awk '{print $5}' | tail -n 1 | sed 's/%//')
-    if [ $USAGE -gt 50 ]; then
-      echo "已用存储空间大于50%！重启BrokerStartup"
-      if [ $testMyRocketMQ -eq 1 ]; then
-        restartRocketMQ $myRocketMQPath
-      else
-        restartRocketMQ $originRocketMQPath
-      fi
+      restartRocketMQ
     fi
     configFilename="rmq$i-1-$j.json"
     resultFilename="result-$testTarget-async-producer.txt"
@@ -90,3 +77,27 @@ for i in 8 16 32 64; do
   done
 done
 
+
+##################
+# async rocketmq
+testTarget="async-rocketmq"
+
+cp /root/broker-async.conf $RocketMQPath/conf/broker.conf
+restartRocketMQ
+
+# async producer
+# 外层循环遍历数字i，i的取值为8、16、32、64
+for i in 1 4 8; do
+  # 内层循环遍历数字j，j的取值为1024、4096、8192、16384、32768、65536、131072、1048576
+  for j in 1024 4096 8192 16384 32768 65536 131072 1048576; do
+    USAGE=$(df -h $RocketMQLogDir | awk '{print $5}' | tail -n 1 | sed 's/%//')
+    if [ $USAGE -gt 50 ]; then
+      echo "已用存储空间大于50%！重启BrokerStartup"
+      restartRocketMQ
+    fi
+    configFilename="rmq$i-1-$j.json"
+    resultFilename="result-$testTarget-async-producer.txt"
+    cd $path
+    doTest $configFilename $resultFilename
+  done
+done
